403bc94a8b9c0e02bfd13f1546bc8237b00c1c1a
Iliyan <i.traykov@esof.net>
2019-11-29 15:26:37 +0200

    123

--- www/FCMPlugin.js
+++ www/FCMPlugin.js
@@ -7,48 +7,48 @@ function FCMPlugin() {
 
 // GET TOKEN //
 FCMPlugin.prototype.getToken = function (success, error) {
-	exec(success, error, "FCMPlugin", 'getToken', []);
+	console.log(124)
 }
 
-// SUBSCRIBE TO TOPIC //
-FCMPlugin.prototype.subscribeToTopic = function (topic, success, error) {
-	exec(success, error, "FCMPlugin", 'subscribeToTopic', [topic]);
-}
-// UNSUBSCRIBE FROM TOPIC //
-FCMPlugin.prototype.unsubscribeFromTopic = function (topic, success, error) {
-	exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
-}
+// // SUBSCRIBE TO TOPIC //
+// FCMPlugin.prototype.subscribeToTopic = function (topic, success, error) {
+// 	exec(success, error, "FCMPlugin", 'subscribeToTopic', [topic]);
+// }
+// // UNSUBSCRIBE FROM TOPIC //
+// FCMPlugin.prototype.unsubscribeFromTopic = function (topic, success, error) {
+// 	exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
+// }
 
-// NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onNotification = function (callback, success, error) {
-	FCMPlugin.prototype.onNotificationReceived = callback;
-	exec(success, error, "FCMPlugin", 'registerNotification', []);
-}
-// DEFAULT NOTIFICATION CALLBACK //
-FCMPlugin.prototype.onNotificationReceived = function (payload) {
-	console.log("fcmTrack Received push notification payload:")
-	console.log(payload)
-}
-//Edit start wed june 22
-// FCM ID CALLBACK //
-FCMPlugin.prototype.onGotFCMID = function (callback, success, error) {
-	FCMPlugin.prototype.onFCMIDReceived = callback;
-	exec(success, error, "FCMPlugin", 'getFCMID', []);
-}
-// DEFAULT ID CALLBACK //
-FCMPlugin.prototype.onFCMIDReceived = function (fcmID) {
-	console.log("fcmTrack Received fcmID:")
-	console.log(fcmID)
-}
-//Edit end
+// // NOTIFICATION CALLBACK //
+// FCMPlugin.prototype.onNotification = function (callback, success, error) {
+// 	FCMPlugin.prototype.onNotificationReceived = callback;
+// 	exec(success, error, "FCMPlugin", 'registerNotification', []);
+// }
+// // DEFAULT NOTIFICATION CALLBACK //
+// FCMPlugin.prototype.onNotificationReceived = function (payload) {
+// 	console.log("fcmTrack Received push notification payload:")
+// 	console.log(payload)
+// }
+// //Edit start wed june 22
+// // FCM ID CALLBACK //
+// FCMPlugin.prototype.onGotFCMID = function (callback, success, error) {
+// 	FCMPlugin.prototype.onFCMIDReceived = callback;
+// 	exec(success, error, "FCMPlugin", 'getFCMID', []);
+// }
+// // DEFAULT ID CALLBACK //
+// FCMPlugin.prototype.onFCMIDReceived = function (fcmID) {
+// 	console.log("fcmTrack Received fcmID:")
+// 	console.log(fcmID)
+// }
+// //Edit end
 
-FCMPlugin.prototype.setBadgeNumber = function (number, success, error) {
-	exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
-};
+// FCMPlugin.prototype.setBadgeNumber = function (number, success, error) {
+// 	exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
+// };
 
-FCMPlugin.prototype.getBadgeNumber = function (success, error) {
-	exec(success, error, "FCMPlugin", "getBadgeNumber", []);
-};
+// FCMPlugin.prototype.getBadgeNumber = function (success, error) {
+// 	exec(success, error, "FCMPlugin", "getBadgeNumber", []);
+// };
 alert(2);
 // FIRE READY //
 //exec(function (result) { console.log("fcmTrack FCMPlugin Ready OK") }, function (result) { console.log("fcmTrack FCMPlugin Ready ERROR") }, "FCMPlugin", 'ready', []);
