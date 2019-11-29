b160e07c30fc0ed8c2875cdf8b3d214fc4fc456c
Iliyan <i.traykov@esof.net>
2019-11-29 17:01:00 +0200

    123

--- www/Fcm.js
+++ www/Fcm.js
@@ -1,56 +1,32 @@
-var exec = require('cordova/exec');
-
 function Fcm() {
-	console.log("FCMPlugin.js: is created");
 }
 
-// GET TOKEN //
-Fcm.prototype.getToken = function (success, error) {
-	console.log(124)
-}
+// Override this method (after deviceready) to set the location where you want the iPad popup arrow to appear.
+// If not overridden with different values, the popup is not used. Example:
+//
+//   window.plugins.socialsharing.iPadPopupCoordinates = function() {
+//     return "100,100,200,300";
+//   };
+Fcm.prototype.iPadPopupCoordinates = function () {
+  // left,top,width,height
+  return "-1,-1,-1,-1";
+};
 
-// // SUBSCRIBE TO TOPIC //
-// FCMPlugin.prototype.subscribeToTopic = function (topic, success, error) {
-// 	exec(success, error, "FCMPlugin", 'subscribeToTopic', [topic]);
-// }
-// // UNSUBSCRIBE FROM TOPIC //
-// FCMPlugin.prototype.unsubscribeFromTopic = function (topic, success, error) {
-// 	exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
-// }
 
-// // NOTIFICATION CALLBACK //
-// FCMPlugin.prototype.onNotification = function (callback, success, error) {
-// 	FCMPlugin.prototype.onNotificationReceived = callback;
-// 	exec(success, error, "FCMPlugin", 'registerNotification', []);
-// }
-// // DEFAULT NOTIFICATION CALLBACK //
-// FCMPlugin.prototype.onNotificationReceived = function (payload) {
-// 	console.log("fcmTrack Received push notification payload:")
-// 	console.log(payload)
-// }
-// //Edit start wed june 22
-// // FCM ID CALLBACK //
-// FCMPlugin.prototype.onGotFCMID = function (callback, success, error) {
-// 	FCMPlugin.prototype.onFCMIDReceived = callback;
-// 	exec(success, error, "FCMPlugin", 'getFCMID', []);
-// }
-// // DEFAULT ID CALLBACK //
-// FCMPlugin.prototype.onFCMIDReceived = function (fcmID) {
-// 	console.log("fcmTrack Received fcmID:")
-// 	console.log(fcmID)
-// }
-// //Edit end
+Fcm.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
 
-// FCMPlugin.prototype.setBadgeNumber = function (number, success, error) {
-// 	exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
-// };
+  window.plugins.fcm = new Fcm();
 
-// FCMPlugin.prototype.getBadgeNumber = function (success, error) {
-// 	exec(success, error, "FCMPlugin", "getBadgeNumber", []);
-// };
+  // Note only polyfill navigator.share if it is not defined, since shareW3C implements L1 of the spec,
+  // and an existing navigator.share method could implement L2.
+  if (!navigator.share) {
+    navigator.share = window.plugins.fcm.shareW3C;
+  }
 
-// FIRE READY //
-//exec(function (result) { console.log("fcmTrack FCMPlugin Ready OK") }, function (result) { console.log("fcmTrack FCMPlugin Ready ERROR") }, "FCMPlugin", 'ready', []);
+  return window.plugins.fcm;
+};
 
-var fcm = new Fcm();
-module.exports = fcm;
+cordova.addConstructor(Fcm.install);
