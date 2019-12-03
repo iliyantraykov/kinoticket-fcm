e4072580f76dd85612196c486ccfac214bcff74f
Iliyan <i.traykov@esof.net>
2019-12-03 10:56:42 +0200

    1

--- www/Fcm.js
+++ www/Fcm.js
@@ -1,17 +1,56 @@
+var exec = require('cordova/exec');
+
 function Fcm() {
+  alert("FCMPlugin.js: is created");
 }
 
-// Override this method (after deviceready) to set the location where you want the iPad popup arrow to appear.
-// If not overridden with different values, the popup is not used. Example:
-//
-//   window.plugins.socialsharing.iPadPopupCoordinates = function() {
-//     return "100,100,200,300";
-//   };
 Fcm.prototype.getToken = function () {
   // left,top,width,height
-  console.log(123);
-  return "-1,-1,-1,-1";
+  return "12345678";
 };
+
+// SUBSCRIBE TO TOPIC //
+Fcm.prototype.subscribeToTopic = function (topic, success, error) {
+	exec(success, error, "FCMPlugin", 'subscribeToTopic', [topic]);
+}
+// UNSUBSCRIBE FROM TOPIC //
+Fcm.prototype.unsubscribeFromTopic = function (topic, success, error) {
+	exec(success, error, "FCMPlugin", 'unsubscribeFromTopic', [topic]);
+}
+
+// NOTIFICATION CALLBACK //
+Fcm.prototype.onNotification = function (callback, success, error) {
+	Fcm.prototype.onNotificationReceived = callback;
+	exec(success, error, "FCMPlugin", 'registerNotification', []);
+}
+// DEFAULT NOTIFICATION CALLBACK //
+Fcm.prototype.onNotificationReceived = function (payload) {
+	console.log("fcmTrack Received push notification payload:")
+	console.log(payload)
+}
+//Edit start wed june 22
+// FCM ID CALLBACK //
+Fcm.prototype.onGotFCMID = function (callback, success, error) {
+	Fcm.prototype.onFCMIDReceived = callback;
+	exec(success, error, "FCMPlugin", 'getFCMID', []);
+}
+// DEFAULT ID CALLBACK //
+Fcm.prototype.onFCMIDReceived = function (fcmID) {
+	console.log("fcmTrack Received fcmID:")
+	console.log(fcmID)
+}
+//Edit end
+
+Fcm.prototype.setBadgeNumber = function (number, success, error) {
+	exec(success, error, "FCMPlugin", "setBadgeNumber", [number]);
+};
+
+Fcm.prototype.getBadgeNumber = function (success, error) {
+	exec(success, error, "FCMPlugin", "getBadgeNumber", []);
+};
+
+// FIRE READY //
+exec(function (result) { console.log("fcmTrack FCMPlugin Ready OK") }, function (result) { console.log("fcmTrack FCMPlugin Ready ERROR") }, "FCMPlugin", 'ready', []);
 
 
 Fcm.install = function () {
